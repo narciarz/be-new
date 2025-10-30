e6392d8274d4a97d1e846928b299430395012089
Marcin Wiewiora <wiewmarcin@gmail.com>
2025-10-30 17:47:22 +0100

    Added backend + git ignore

---
+++ benew-services/src/main/java/com/narciarz/benew/BenewApplication.java
@@ -0,0 +1,13 @@
+package com.narciarz.benew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenewApplication.class, args);
+	}
+
+}
---
+++ benew-services/src/test/java/com/narciarz/benew/BenewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.narciarz.benew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BenewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
