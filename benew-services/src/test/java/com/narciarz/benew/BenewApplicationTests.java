caddf9ec23eb7eb3774e2872ffb316c8a94b7896
Marcin Wiewiora <wiewmarcin@gmail.com>
2025-11-05 22:59:50 +0100

    added testcontainers to load context properly

--- benew-services/src/test/java/com/narciarz/benew/BenewApplicationTests.java
+++ benew-services/src/test/java/com/narciarz/benew/BenewApplicationTests.java
@@ -2,9 +2,24 @@ package com.narciarz.benew;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
+@ActiveProfiles("test")
 class BenewApplicationTests {
+
+	@Container
+	@ServiceConnection
+	@SuppressWarnings("resource") // Container lifecycle is managed by Testcontainers
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine")
+			.withDatabaseName("benew_test")
+			.withUsername("test")
+			.withPassword("test");
 
 	@Test
 	void contextLoads() {
