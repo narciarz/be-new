548f44f7f9c1367ed74fc8c1588c8ed0e0534b03
Marcin Wiewiora <wiewmarcin@gmail.com>
2025-11-10 23:32:07 +0100

    security bypass

--- benew-services/src/main/java/com/narciarz/benew/config/SecurityConfig.java
+++ benew-services/src/main/java/com/narciarz/benew/config/SecurityConfig.java
@@ -2,8 +2,13 @@ package com.narciarz.benew.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * Security configuration for the application.
@@ -12,6 +17,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  * with JWT authentication and role-based access control configuration.</p>
  */
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     
     /**
@@ -25,5 +31,15 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
+        return http.build();
     }
 }
